2b81f234ff5f77441005ee559537ebc003f0004a
Roisin Brogan <roisinorourkebrogan@gmail.com>
2024-09-06 15:32:18 +0200

    package altered to work with Nico's model: requirements.txt -> tensorflow=2.17.0, setup.py -> version 0.2.0, resize to 81 by 81, rather than 224 by 224. PROBLEM? the model predicts the wrong plant

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open("requirements.txt") as f:
 requirements = [x.strip() for x in content if "git+" not in x]
 
 setup(name='ai_weeder_package',
-      version="0.1.0",
-      description="Plant Identifcation Model (api_pred)",
+      version="0.2.0",
+      description="Plant Identification Model (api_pred)",
       #license="",
       author="AI Weeder Group",
       #author_email="",
       #url="https://github.com/dorwein/ai_weeder",
       install_requires=requirements,
       packages=find_packages(),
-      test_suite="tests",
-      # include_package_data: to install data from MANIFEST.in
+      #test_suite="tests",
+      #include_package_data: to install data from MANIFEST.in
       include_package_data=True,
       zip_safe=False)
